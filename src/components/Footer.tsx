fa5c379ce34111aae8c67ebb327eaca059c645b8
Trong An <troandev@gmail.com>
2025-03-08 08:51:12 +0700

    fix: update footer to credit author and change link to GitHub profile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,16 @@ export default function Footer() {
     return (
         <footer className="bg-white/70 border-t border-t-gray-300 dark:border-t-white/10 text-gray-900 p-4 dark:bg-slate-800 dark:text-white">
             <div className=" text-center">
-                © 2025{" "}
-                <a href="https://www.facebook.com/trongandev" className="text-blue-700 underline" target="_blank">
+                © 2025 edit and optimize by{" "}
+                <a href="https://github.com/angutboiz" className="text-blue-700 underline" target="_blank">
                     trongandev
                 </a>
-                . All rights reserved.
+            </div>
+            <div className="text-center">
+                Author:{" "}
+                <a href="https://github.com/BWCbewchan" className="underline" target="_blank">
+                    Trần Chí Bảo{" "}
+                </a>
             </div>
         </footer>
     );
