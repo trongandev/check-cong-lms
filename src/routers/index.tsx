83c37e0ac7f072605836fd00735052df28683f48
Trong An <you@example.com>
2025-09-08 11:12:19 +0700

    update t8

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,18 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
-import ThangHai from "../pages/thang2";
-import ThangBa from "../pages/thang3";
-import Thang4 from "@/pages/thang4";
-import Thang5 from "@/pages/thang5";
-import Thang6 from "@/pages/thang6";
-import Thang7 from "@/pages/thang7";
+import { createBrowserRouter } from "react-router-dom"
+import App from "../App"
+import ThangHai from "../pages/thang2"
+import ThangBa from "../pages/thang3"
+import Thang4 from "@/pages/thang4"
+import Thang5 from "@/pages/thang5"
+import Thang6 from "@/pages/thang6"
+import Thang7 from "@/pages/thang7"
+import Thang8 from "@/pages/thang8"
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
-            { path: "", element: <Thang7 /> },
+            { path: "", element: <Thang8 /> },
             { path: "thang6", element: <Thang6 /> },
             { path: "thang5", element: <Thang5 /> },
             { path: "thang4", element: <Thang4 /> },
@@ -20,6 +21,6 @@ const router = createBrowserRouter([
             { path: "thang3", element: <ThangBa /> },
         ],
     },
-]);
+])
 
-export default router;
+export default router
