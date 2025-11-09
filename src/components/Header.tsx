7232b96e748a92ddf40a9f55f6aa493f82f22ced
Trong An <troandev@gmail.com>
2025-11-09 09:21:08 +0700

    add new

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,11 +27,6 @@ export default function Header() {
         setIsDark(!isDark);
     };
 
-    // const handleClearCache = () => {
-    //     localStorage.removeItem("thang3");
-    //     window.location.reload();
-    // };
-
     return (
         <div className="dark:bg-slate-800 bg-white dark:text-white w-full h-13 shadow-md border-b border-gray-200 dark:border-b-white/10  flex items-center justify-center px-5 md:px-0">
             <div className="w-full md:w-[800px] lg:w-[1000px] xl:w-[1200px] flex justify-between items-center">
@@ -40,28 +35,10 @@ export default function Header() {
                 </a>
                 <div className="flex items-center gap-5">
                     <a href="/" className={`flex items-center gap-1 ${pathname === "/" ? "text-blue-500 font-bold" : ""}`}>
-                        <Calendar size={18} /> Công tháng 7
+                        <Calendar size={18} /> Công tháng 10
                     </a>
-                    {/* <a href="/" className={`flex items-center gap-1 ${pathname === "/" ? "text-blue-500 font-bold" : ""}`}>
-                        <Calendar size={18} /> Công tháng 6
-                    </a> */}
-                    {/* <a href="/thang4" className={`flex items-center gap-1 ${pathname === "/thang4" ? "text-blue-500 font-bold" : ""}`}>
-                        <Calendar size={18} /> Công tháng 4
-                    </a>
-                    <a href="/thang3" className={`flex items-center gap-1 ${pathname === "/thang3" ? "text-blue-500 font-bold" : ""}`}>
-                        <Calendar size={18} /> Công tháng 3
-                    </a>
-                    <a href="/thang2" className={`flex items-center gap-1 ${pathname === "/thang2" ? "text-blue-500 font-bold" : ""}`}>
-                        <Calendar size={18} /> Công tháng 2
-                    </a> */}
                 </div>
                 <div className="flex items-center gap-3">
-                    {/* <div className="">
-                        <Button onClick={handleClearCache}>
-                            <CloudDownload />
-                            Cập nhật dữ liệu mới
-                        </Button>
-                    </div> */}
                     <div className="flex items-center gap-1">
                         <button onClick={toggleTheme} className="p-2 cursor-pointer hover:bg-gray-100 dark:hover:bg-slate-700 rounded-full">
                             {isDark ? <Sun size={20} /> : <Moon size={20} />}
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,24 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import ThangHai from "../pages/thang2";
-import ThangBa from "../pages/thang3";
-import Thang4 from "@/pages/thang4";
-import Thang5 from "@/pages/thang5";
-import Thang6 from "@/pages/thang6";
-import Thang7 from "@/pages/thang7";
+
+import TableOffficeHourPage from "@/pages/TableOffficeHourPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-        children: [
-            { path: "", element: <Thang7 /> },
-            { path: "thang6", element: <Thang6 /> },
-            { path: "thang5", element: <Thang5 /> },
-            { path: "thang4", element: <Thang4 /> },
-            { path: "thang2", element: <ThangHai /> },
-            { path: "thang3", element: <ThangBa /> },
-        ],
+        children: [{ path: "", element: <TableOffficeHourPage file="t10-2025" /> }],
     },
 ]);
 
